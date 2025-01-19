147ca8a7316d0bac68120574e14f7d47b63dbe5b
Jared Coglianese <cog20001@byui.edu>
2025-01-18 17:48:49 -0700

    Did prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 class Program
 {
+    static int number;
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numberList = new List<int>();
+        Console.WriteLine("Enter a list of numbers type 0 when finished.");
+        do
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+            if (number != 0)
+            {
+                numberList.Add(number);
+            }
+        } while (number != 0);
+
+        int numberSum = numberList.Sum();
+        double numberAverage = numberList.Average();
+        int largeNumber = numberList.Max();
+        Console.WriteLine($"The sum is: {numberSum} ");
+        Console.WriteLine($"The average is: {numberAverage}");
+        Console.WriteLine($"The largest number is: {largeNumber}");
+
     }
 }
