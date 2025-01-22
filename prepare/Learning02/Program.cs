18d15012fc3fa5f91bad627d6d75224dda9d2968
Jared Coglianese <cog20001@byui.edu>
2025-01-21 22:07:34 -0700

    Did prep 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+    
+       Job job1 = new Job();
+       job1._jobTitle = "Software Engineer";
+
+       Job job2 = new Job();
+       job2._jobTitle = "Garbage Man";
+
+       Resume myResume = new Resume();
+       myResume._name = "Jared Coglianese";
+       myResume._jobs.Add(job1);
+       myResume._jobs.Add(job2);
+       myResume.DisplayResume();
+
+       
+       
     }
 }
