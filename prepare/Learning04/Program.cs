2b697da6c19f0cab9ffb7c9003b8c0474189dd51
Jared Coglianese <cog20001@byui.edu>
2025-02-21 20:47:01 -0700

    Finished Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assignment1 = new MathAssignment("Samuel Bennett", "Multiplicaton", "7.3", "8-19");
+        Console.WriteLine($"\n{assignment1.GetSummary()}");
+        Console.WriteLine($"{assignment1.GetHomeworkList()}\n");
+
+        WritingAssignment assignment2 = new WritingAssignment("Mary Walters", "European History", "The Cause of World War II");
+        Console.WriteLine($"{assignment2.GetSummary()}");
+        Console.WriteLine($"{assignment2.GetWritingInformation()}\n");
     }
 }
